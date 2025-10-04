8d3cc41392b9cd9027c80f426dc03dab5e746836
Mateo Acuña <mateogabrielacuna@gmail.com>
2025-10-04 12:28:16 -0300

    Se agregan primer commit con archivos para pruebas conexion db

---
+++ main.py
@@ -0,0 +1,13 @@
+from db import engine
+from db_conection import get_db
+
+def test_connection():
+    try:
+        with engine.connect() as conn:
+            result = conn.execute("SELECT NOW();")
+            print("✅ Conexión exitosa:", result.fetchone())
+    except Exception as e:
+        print("Error al conectar:", e)
+
+if __name__ == "__main__":
+    test_connection()
